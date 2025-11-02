1424ae666406290fd7292180783cd36c3a8d6598
wolf <douglaswolf@yahoo.com>
2025-11-01 17:54:36 -0700

    No commit message

--- src/axil_slave.v
+++ src/axil_slave.v
@@ -5,6 +5,8 @@
 //   Date     Who   Ver  Changes
 //====================================================================================
 // 16-Dec-23  DWW     1  Initial creation
+//
+// 22-Jul-25  DWW     2  Updated to accomodate updated axi4_lite_slave.v
 //====================================================================================
 
 /*
@@ -24,13 +26,14 @@ module axil_slave # (parameter AW=8)
     // "Specify write address"              -- Master --    -- Slave --
     input[AW-1:0]                           S_AXI_AWADDR,   
     input                                   S_AXI_AWVALID,  
+    input[   2:0]                           S_AXI_AWPROT,
     output                                                  S_AXI_AWREADY,
-    input[2:0]                              S_AXI_AWPROT,
+
 
     // "Write Data"                         -- Master --    -- Slave --
     input[31:0]                             S_AXI_WDATA,      
     input                                   S_AXI_WVALID,
-    input[3:0]                              S_AXI_WSTRB,
+    input[ 3:0]                             S_AXI_WSTRB,
     output                                                  S_AXI_WREADY,
 
     // "Send Write Response"                -- Master --    -- Slave --
@@ -40,14 +43,14 @@ module axil_slave # (parameter AW=8)
 
     // "Specify read address"               -- Master --    -- Slave --
     input[AW-1:0]                           S_AXI_ARADDR,     
+    input[   2:0]                           S_AXI_ARPROT,     
     input                                   S_AXI_ARVALID,
-    input[2:0]                              S_AXI_ARPROT,     
     output                                                  S_AXI_ARREADY,
 
     // "Read data back to master"           -- Master --    -- Slave --
     output[31:0]                                            S_AXI_RDATA,
     output                                                  S_AXI_RVALID,
-    output[1:0]                                             S_AXI_RRESP,
+    output[ 1:0]                                            S_AXI_RRESP,
     input                                   S_AXI_RREADY
     //==========================================================================
 );  
@@ -63,20 +66,20 @@ localparam REG_SUM      = 2;
 // We'll communicate with the AXI4-Lite Slave core with these signals.
 //==========================================================================
 // AXI Slave Handler Interface for write requests
-wire[31:0]  ashi_windx;     // Input   Write register-index
-wire[31:0]  ashi_waddr;     // Input:  Write-address
-wire[31:0]  ashi_wdata;     // Input:  Write-data
-wire        ashi_write;     // Input:  1 = Handle a write request
-reg[1:0]    ashi_wresp;     // Output: Write-response (OKAY, DECERR, SLVERR)
-wire        ashi_widle;     // Output: 1 = Write state machine is idle
+wire[  31:0]  ashi_windx;     // Input   Write register-index
+wire[AW-1:0]  ashi_waddr;     // Input:  Write-address
+wire[  31:0]  ashi_wdata;     // Input:  Write-data
+wire          ashi_write;     // Input:  1 = Handle a write request
+reg [   1:0]  ashi_wresp;     // Output: Write-response (OKAY, DECERR, SLVERR)
+wire          ashi_widle;     // Output: 1 = Write state machine is idle
 
 // AXI Slave Handler Interface for read requests
-wire[31:0]  ashi_rindx;     // Input   Read register-index
-wire[31:0]  ashi_raddr;     // Input:  Read-address
-wire        ashi_read;      // Input:  1 = Handle a read request
-reg[31:0]   ashi_rdata;     // Output: Read data
-reg[1:0]    ashi_rresp;     // Output: Read-response (OKAY, DECERR, SLVERR);
-wire        ashi_ridle;     // Output: 1 = Read state machine is idle
+wire[  31:0]  ashi_rindx;     // Input   Read register-index
+wire[AW-1:0]  ashi_raddr;     // Input:  Read-address
+wire          ashi_read;      // Input:  1 = Handle a read request
+reg [  31:0]  ashi_rdata;     // Output: Read data
+reg [   1:0]  ashi_rresp;     // Output: Read-response (OKAY, DECERR, SLVERR);
+wire          ashi_ridle;     // Output: 1 = Read state machine is idle
 //==========================================================================
 
 // The state of the state-machines that handle AXI4-Lite read and AXI4-Lite write
@@ -90,9 +93,6 @@ assign ashi_ridle = (ashi_read  == 0) && (ashi_read_state  == 0);
 localparam OKAY   = 0;
 localparam SLVERR = 2;
 localparam DECERR = 3;
-
-// The address mask is 'AW' 1-bits in a row
-localparam ADDR_MASK = (1 << AW) - 1;
 
 // These are the two numbers that we're going to add together
 reg[31:0] addend1, addend2;
@@ -135,8 +135,6 @@ end
 
 
 
-
-
 //==========================================================================
 // World's simplest state machine for handling AXI4-Lite read requests
 //==========================================================================
@@ -173,13 +171,14 @@ end
 //==========================================================================
 // This connects us to an AXI4-Lite slave core
 //==========================================================================
-axi4_lite_slave#(ADDR_MASK) i_axi4lite_slave
+axi4_lite_slave#(.AW(AW)) i_axi4lite_slave
 (
     .clk            (clk),
     .resetn         (resetn),
     
     // AXI AW channel
     .AXI_AWADDR     (S_AXI_AWADDR),
+    .AXI_AWPROT     (S_AXI_AWPROT),
     .AXI_AWVALID    (S_AXI_AWVALID),   
     .AXI_AWREADY    (S_AXI_AWREADY),
     
@@ -196,6 +195,7 @@ axi4_lite_slave#(ADDR_MASK) i_axi4lite_slave
 
     // AXI AR channel
     .AXI_ARADDR     (S_AXI_ARADDR), 
+    .AXI_ARPROT     (S_AXI_ARPROT),
     .AXI_ARVALID    (S_AXI_ARVALID),
     .AXI_ARREADY    (S_AXI_ARREADY),
 
